68725c846d5b744102710e7df02248689de7866a
CUBERCODE-2022 <sepehrnikobin1389@gmail.com>
2022-08-29 13:02:28 +0430

    commit for repository

---
+++ pages/page2.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>وبیوتی</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <style>
+        *{
+            box-sizing: border-box;
+            margin: 3px;
+            padding:0px;
+        }
+        .serch1{
+    display: block;
+    border: none;
+    width:300px;
+    padding: 6px;
+}
+
+.serch2{
+    background-color:#2ecc71;
+    color: #fff;
+    width: 100px;
+    display: block;
+    border: none;
+    margin-top: 10px;
+    padding:6px;
+    border-radius: 5px;
+    font-weight: bold;
+}
+.serch2:hover{
+    background-color: #95a5a6;
+    cursor: pointer;
+}
+.error{
+    color: red;
+    font-size: 20px;
+    margin-bottom: -30px;
+}
+.title{
+    background: chartreuse;
+    text-align: center;
+    color:#fff;
+    font-size: 20px;
+    padding: 3px;
+}
+    </style>
+</head>
+<body dir="rtl">
+        <div class="title">وبیوتی</div>
+    <div class="main">
+        <form>
+            <h4>نام و نام خانوادگی فرد مورد نظر:</h4>
+            <input class="serch1" type="text" name="nameandfamily" placeholder="نام و نام خانوادگی فرد مورد نظر">
+            <input class="serch2" type="submit" value="جستجو">
+    </form>
+    </div>
+    <?php
+        echo "<h3>نتایج :</h3>";
+        if(isset($_GET['nameandfamily'])){
+        $x=$_GET['nameandfamily'];
+        
+        switch ($x){
+            case 'فرید نیکوبین':
+                echo "$x"."<br/>".'کد ملی: 4650420083'."<br/>".'شماره تلفن: 09131824063'."<br/>"."تاریخ تولد: 1355/6/8"."<br/>".'محل زندگی: بروجن'."<br/>".'نام پدر: محمد صادق';
+            break;
+           case 'سپهر نیکوبین':
+                echo "$x"."<br/>".'کد ملی: 4640393598'."<br/>".'شماره تلفن: 09162914063'."<br/>".'تاریخ تولد: 1401/4/18'.'<br/>'.'محل زندگی: بروجن'."<br/>".'نام پدر: فرید'."<br/>".'(مدیر سایت)';
+            break;
+            case 'منیژه کرمی':
+                echo "$x"."<br/>".'کد ملی: 4650122864'."<br/>".'شماره تلفن: 09132847947'."<br/>".'تاریخ تولد: 1361/11/28'."<br/>".'محل زندگی: بروجن'."<br/>".'نام پدر: جهانگیر';
+            break;
+            case 'پوریا نیکوبین':
+                echo "$x"."<br/>".'کد ملی: 4640503997'."<br/>".'شماره تلفن: ندارد'."<br/>".'تاریخ تولد: 1395/3/7'."<br/>".'محل زندگی: بروجن'."<br/>".'نام پدر: فرید';
+            break;
+            case '':
+                echo ""."<br/>".''."<br/>".''."<br/>".''."<br/>".''."<br/>".'';
+            break;
+            case '':
+                echo ""."<br/>".''."<br/>".''."<br/>".''."<br/>".''."<br/>".'';
+            break;
+            case '':
+                echo ""."<br/>".''."<br/>".''."<br/>".''."<br/>".''."<br/>".'';
+            break;
+            case '':
+                echo ""."<br/>".''."<br/>".''."<br/>".''."<br/>".''."<br/>".'';
+            break;
+            case '':
+                echo ""."<br/>".''."<br/>".''."<br/>".''."<br/>".''."<br/>".'';
+            break;
+            default:
+                echo '<div class="error">متاسفیم</div>'."<br/>".'فرد مورد نظر شما در سایت ثبت نشده است. میتوانید با این شماره: 09162914063 تماس حاصل فرمایید و با ما مشاوره کنید.';
+        }
+    }
+    ?>
+</body>
+</html>
