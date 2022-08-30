a9c557b68be62abcdf442fe1923d436b44a5473b
CUBERCODE-2022 <sepehrnikobin1389@gmail.com>
2022-08-30 18:26:40 +0430

    Make a error in the onpage

--- pages/page1.php
+++ pages/page1.php
@@ -11,19 +11,26 @@
             color:#2ecc71;
         }
         .on{
-            margin-top: 100px;
             text-decoration: none;
             color:#fff;
             background-color: #2ecc71;           
             font-size: 12px;
-            padding: 8px;
+            padding: 10px;
             border-radius:5px;
-            height: 12px;
+            height: 15px;
             font-weight: bold;
         }
         .on:hover{
             cursor: pointer;
             background-color: #95a5a6;
+        }
+
+        .error{
+            text-align: center;
+            color: red;
+            font-size: 20px;
+            background-color: black;
+            padding: 8px;
         }
     </style>
 </head>
@@ -32,16 +39,23 @@
     <?php
         $name=$_POST['name']??'';
         $age=$_POST['age']??'';
-        $numphon=$_POST['phonnumber']??'';
+        $phonenum=$_POST['phonnumber']??'';
         $NationalCode=$_POST['NationalCode']??'';
         $gender=$_POST['gender']??'';
-        if($NationalCode==null)
-        echo '<h2 class="th">ثبت نام شما با موفقیت انجام شد</h2>';
-        echo "$name"."&nbsp;"."&nbsp;"."عزیز"; 
-        echo "<h4>برای ورود به سایت کافیست رو دکمه زیر کلیک کنید.</h4>";
+        
+        if($phonenum || $gender || $age || $name || $NationalCode==null){
+            echo '<h2 class="error">'.'لطفا'."&nbsp".'همه'."&nbsp;".'اطلاعات'."&nbsp".'خود'."&nbsp".'را'."&nbsp".'به'."&nbsp".'درستی'."&nbsp".'کنید.'.'<h2/>';
+            echo "<br/>".'<a class="on" href="../index.html">'.'باشه'.'</a>';
+        }else{
+            echo '<h2 class="th">ثبت نام شما با موفقیت انجام شد</h2>';
+            echo "$name"."&nbsp;"."&nbsp;"."عزیز"; 
+            echo "<h4>برای ورود به سایت کافیست رو دکمه زیر کلیک کنید.</h4>";
+            echo '<a href="page2.php" class="on">ورود</a>';
+        }
+
     ?>    
         
-    <a href="page2.php" class="on">ورود</a>
+    
     
      
 </body>
