d60dcfccb7efbbedd60ae80207059bfa4f37840e
CUBERCODE-2022 <sepehrnikobin1389@gmail.com>
2022-09-04 16:50:32 +0430

    replace the files for make good project

--- pages/pages/page1.php
+++ pages/pages/page1.php
@@ -35,7 +35,7 @@
     </style>
 </head>
 <body dir="rtl" style="margin: 10px;">
-
+        
     <?php
         $name=$_POST['name']??'';
         $age=$_POST['age']??'';
@@ -43,20 +43,18 @@
         $NationalCode=$_POST['NationalCode']??'';
         $gender=$_POST['gender']??'';
         
-        if($phonenum || $gender || $age || $name || $NationalCode==null){
-            echo '<h2 class="error">'.'لطفا'."&nbsp".'همه'."&nbsp;".'اطلاعات'."&nbsp".'خود'."&nbsp".'را'."&nbsp".'به'."&nbsp".'درستی'."&nbsp".'کنید.'.'<h2/>';
-            echo "<br/>".'<a class="on" href="../index.html">'.'باشه'.'</a>';
-        }else{
+        if($phonenum || $gender || $age || $name || $NationalCode==!null){
             echo '<h2 class="th">ثبت نام شما با موفقیت انجام شد</h2>';
             echo "$name"."&nbsp;"."&nbsp;"."عزیز"; 
             echo "<h4>برای ورود به سایت کافیست رو دکمه زیر کلیک کنید.</h4>";
             echo '<a href="page2.php" class="on">ورود</a>';
+        }else{
+            echo '<h2 class="error">'.'لطفا'."&nbsp".'همه'."&nbsp;".'اطلاعات'."&nbsp".'خود'."&nbsp".'را'."&nbsp".'به'."&nbsp".'درستی'."&nbsp".'وارد'."&nbsp;".'کنید.'.'<h2/>';
+            echo "<br/>".'<a class="on" href="../index.html">'.'باشه'.'</a>';
         }
 
-    ?>    
+    ?>
         
-    
-    
      
 </body>
 </html>
